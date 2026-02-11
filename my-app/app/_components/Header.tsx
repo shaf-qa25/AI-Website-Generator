4f603c981bcd675586cbf6464f5b3f37ce868ca2
Shafqa Fatma <fatmashafqa4@gmail.com>
2026-02-11 18:59:42 +0530

    Created Hero Section

---
+++ my-app/app/_components/Header.tsx
@@ -0,0 +1,40 @@
+import { Button } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
+import Image from 'next/image'
+import React from 'react'
+
+
+const MenuOption = [
+    {
+        name: 'Pricing',
+        path: '/pricing'
+    },
+    {
+        name: 'Contact Us',
+        path: '/contact'
+    }
+]
+function Header() {
+    return (
+        <div className="flex items-center justify-between py-6 px-10 bg-white dark:bg-black shadow-md">
+            {/* logo/ */}
+            <div className='flex items-center gap-2 font-bold text-lg'>
+                <Image src={'/logo.svg'} alt="logo" width={35} height={35} />
+                <h2>AI Website Generator</h2>
+            </div>
+            {/* menu */}
+            <div className='flex gap-2'>
+                {MenuOption.map((options, index) => {
+                    return (
+                        <Button variant="ghost" key={index}>{options.name}</Button>
+                    )
+
+                })}
+            </div>
+            {/* get started */}
+            <Button >Get Started <ArrowRight /></Button>
+        </div>
+    )
+}
+
+export default Header
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      texting..............
-    </div>
+    <Header />
   );
 }
